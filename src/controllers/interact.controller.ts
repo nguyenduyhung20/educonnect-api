7237bd080d3d350eb4a705936b309f3faa647f97
DHG <duyhung8a2@gmail.com>
2023-10-30 05:09:22 +0700

    feat: interact crud

---
+++ src/controllers/interact.controller.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from 'express';
+import { InteractModel } from '../models/interact.model';
+
+export const handleGetPostInteract = async (req: Request, res: Response, next: NextFunction) => {
+  const { requestPost } = req;
+  try {
+    const users = await InteractModel.getByPostId(requestPost.id);
+    res.status(200).json({ users });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const handleCreatePostInteract = async (req: Request, res: Response, next: NextFunction) => {
+  const { body: postFields } = req;
+  try {
+    const users = await InteractModel.create(postFields);
+    res.status(200).json({ users });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const handleUpdatePostInteract = async (req: Request, res: Response, next: NextFunction) => {
+  const { requestUser, requestPost, body: postFields } = req;
+  try {
+    const users = await InteractModel.update(requestUser.id, requestPost.id, postFields);
+    res.status(200).json({ users });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const handleDeletePostInteract = async (req: Request, res: Response, next: NextFunction) => {
+  const { requestUser, requestPost } = req;
+  try {
+    const users = await InteractModel.delete(requestUser.id, requestPost.id);
+    res.status(200).json({ users });
+  } catch (error) {
+    next(error);
+  }
+};
