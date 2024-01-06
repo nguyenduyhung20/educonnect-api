36f702ca2c4a25c83ed613dc1d677c8a215d3496
hao-phamlehoan <hao.phamlehoan@hcmut.edu.vn>
2024-01-07 01:05:01 +0700

    feature: upload file

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import fileUpload from 'express-fileupload';
 import cookieParser from "cookie-parser";
 import router from './routes/index.route';
 import { handleError } from './middleware/error.middleware';
@@ -10,9 +11,12 @@ const app = express();
 // cors
 app.use(cors());
 
+// file upload
+app.use(fileUpload({limits: { fileSize: 50 * 1024 * 1024 }})); // 50 MB
+
 // body parser
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false, limit: '25mb' }));
 
 // cookie parser
 app.use(cookieParser());
