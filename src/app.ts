d6906fbc1c07b48cd5b180ec6c070912cd8b5e0c
hao-phamlehoan <hao.phamlehoan@hcmut.edu.vn>
2024-01-07 10:12:58 +0700

    refactor

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import fileUpload from 'express-fileupload';
-import cookieParser from "cookie-parser";
+import cookieParser from 'cookie-parser';
 import router from './routes/index.route';
 import { handleError } from './middleware/error.middleware';
 
@@ -12,19 +12,19 @@ const app = express();
 app.use(cors());
 
 // file upload
-app.use(fileUpload({limits: { fileSize: 50 * 1024 * 1024 }})); // 50 MB
+app.use(fileUpload({ limits: { fileSize: 50 * 1024 * 1024 } })); // 50 MB
 
 // body parser
 app.use(express.json());
-app.use(express.urlencoded({ extended: false, limit: '25mb' }));
+app.use(express.urlencoded({ extended: false }));
 
 // cookie parser
 app.use(cookieParser());
 
 // custom middleware should go here
-app.get('/',async (req, res) => {
-    res.send('BKU with love <3');    
-})
+app.get('/', async (req, res) => {
+  res.send('BKU with love <3');
+});
 
 // routes
 app.use('/api', router);
--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
-import fileUpload, { UploadedFile } from 'express-fileupload';
+import { UploadedFile } from 'express-fileupload';
 import { AuthModel } from '../models/auth.model';
 import { uploadFile } from '../utils/uploadFile';
 import { SUCCESS_RESPONSE } from '../constants/success';
@@ -11,7 +11,7 @@ dotenv.config();
 export const handleLogin = async (req: Request, res: Response, next: NextFunction) => {
   const { username, password } = req.body;
   try {
-    const account = await AuthModel.login(username, password);
+    const account = await AuthModel.login(username);
 
     if (!account) {
       return res.status(401).json({ data: 'Username does not exists' });
@@ -54,13 +54,13 @@ export const handleLogin = async (req: Request, res: Response, next: NextFunctio
 // POST account/uuid/:userUuid/follow/following/uuid/:followedUuid
 export const handleRegister = async (req: Request, res: Response, next: NextFunction) => {
   const uploadedFiles = req.files?.uploadedFiles as UploadedFile | UploadedFile[];
-  let { body } = req;
+  const { body } = req;
   try {
     if (uploadedFiles) {
       if (Array.isArray(uploadedFiles)) {
-        for (let file of uploadedFiles) {
+        for (const file of uploadedFiles) {
           body.avatar = await uploadFile(file);
-        };
+        }
       } else {
         body.avatar = await uploadFile(uploadedFiles);
       }
@@ -72,6 +72,6 @@ export const handleRegister = async (req: Request, res: Response, next: NextFunc
   }
 };
 
-export const handleLogout = async (req: Request, res: Response, next: NextFunction) => {
+export const handleLogout = async (req: Request, res: Response) => {
   res.clearCookie('token').status(200).json({ data: 'Successfully logged out' });
 };
