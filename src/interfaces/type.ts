deb493006345f9528a92c4ee388ee8f2f7dde043
Binecsbk20 <bientran@MacBook-Pro-cua-Bien.local>
2024-01-07 09:10:31 +0700

    fix: error

--- src/interfaces/type.ts
+++ src/interfaces/type.ts
@@ -1,16 +1,16 @@
 import type { interact_type, member_role, member_status, user_role, user_sex } from '../databases/enums';
 
 export type RegisterType = {
-    id?: number,
-    address?: string,
-    name?: string,
-    phone?: string,
-    birthday?: Date | string,
-    email?: string,
-    ssn?: string,
-    sex?: user_sex,
-    username?: string,
-    password?: string,
-    avatar?: string,
-    role: user_role
-}
+  id?: number;
+  address?: string;
+  name?: string;
+  phone?: string;
+  birthday?: Date | string;
+  email?: string;
+  ssn?: string;
+  sex?: user_sex;
+  username?: string;
+  password?: string;
+  avatar?: string;
+  role: user_role;
+};
