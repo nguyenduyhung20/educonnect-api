c7b5721c856aacc2991427cc6f2cc695d888faf4
hao-phamlehoan <hao.phamlehoan@hcmut.edu.vn>
2024-01-08 21:51:20 +0700

    fix: refactor code

--- src/services/kafka-client.ts
+++ src/services/kafka-client.ts
@@ -1,46 +1,44 @@
 import { Kafka, Partitioners, EachMessagePayload } from 'kafkajs';
 
-const brokers = ["localhost:29092"]
+const brokers = ['localhost:29092'];
 
-export async function consumer(topic: string, clientId = "kafka-consumer-client") {
-    const kafka = new Kafka({ clientId, brokers })
-    const consumer = kafka.consumer({ groupId: clientId })
-    await consumer.connect()
-    await consumer.subscribe({ topic })
-    await consumer.run({
-        eachMessage: async ({ topic, partition, message }: EachMessagePayload ) => {
-            console.log(`received message: ${topic} ${partition} ${message.value}`)
-        },
-    })
-};
+export async function consumer(topic: string, clientId = 'kafka-consumer-client') {
+  const kafka = new Kafka({ clientId, brokers });
+  const consumer = kafka.consumer({ groupId: clientId });
+  await consumer.connect();
+  await consumer.subscribe({ topic });
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }: EachMessagePayload) => {
+      console.log(`received message: ${topic} ${partition} ${message.value}`);
+    }
+  });
+}
 
-export async function producer(topic: string, messages: any, clientId = "kafka-producer-client") {
-    const kafka = new Kafka({
-        clientId,
-        brokers
-    });
+export async function producer(topic: string, messages: any, clientId = 'kafka-producer-client') {
+  const kafka = new Kafka({
+    clientId,
+    brokers
+  });
 
-    const producer = kafka.producer({
-        createPartitioner: Partitioners.LegacyPartitioner,
-    });
-    await producer.connect();
-    await producer.send({
-        topic,
-        messages: messages
-    })
-};
-
+  const producer = kafka.producer({
+    createPartitioner: Partitioners.LegacyPartitioner
+  });
+  await producer.connect();
+  await producer.send({
+    topic,
+    messages: messages
+  });
+}
 
 // produce()
 //     .then(() => {
-//         console.log("produced successfully");
+//         console.log('produced successfully');
 //     })
 //     .catch((err) => console.log(err));
 
-
 // consume().
-//     then((data) => { 
-//         console.log("consumed successfully") 
+//     then((data) => {
+//         console.log('consumed successfully')
 //         console.log(data)
 //     })
 //     .catch(err => console.log(err));
