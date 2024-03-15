a8f132d9c6f9a50120714e76b0d7f030d0ed1565
DHG <duyhung8a2@gmail.com>
2024-03-15 17:41:38 +0700

    feat: working mongo service in docker

--- mongodb-service/src/index.ts
+++ mongodb-service/src/index.ts
@@ -3,15 +3,16 @@ import { createKafkaConsumer } from './kafka';
 import { connectToMongoDB } from './mongo';
 import { processEvent } from './event';
 
-const MONGO_DOMAIN = process.env.MONGO_DOMAIN ?? 'localhost';
-const KAFKA_DOMAIN = process.env.KAFKA_DOMAIN ?? 'localhost';
+const MONGO_DOMAIN = process.env.MONGO_DOMAIN || 'localhost';
+const KAFKA_DOMAIN = process.env.KAFKA_DOMAIN || 'localhost';
+const KAFKA_PORT = process.env.KAFKA_PORT || '29092';
 
 const mongoURI = `mongodb://dev:password@${MONGO_DOMAIN}:27017`;
-const kafkaBrokers = [`${KAFKA_DOMAIN}:29092`];
+const kafkaBrokers = [`${KAFKA_DOMAIN}:${KAFKA_PORT}`];
 const consumerGroupId = 'mongodb-service';
 
 async function main() {
-  logger.info('MongoDB Service started');
+  logger.info(`MongoDB Service started with ${kafkaBrokers} ${MONGO_DOMAIN}:27017`);
   const collection = await connectToMongoDB(mongoURI);
   const consumer = await createKafkaConsumer(kafkaBrokers, consumerGroupId);
 
--- mongodb-service/src/kafka.ts
+++ mongodb-service/src/kafka.ts
@@ -1,9 +1,10 @@
 import { Consumer, Kafka } from 'kafkajs';
-import { PinoLogCreator } from './logger';
+import { PinoLogCreator, logger } from './logger';
 
 const topic = 'user-events';
 
 export async function createKafkaConsumer(brokers: string[], groupId: string): Promise<Consumer> {
+  logger.info(brokers);
   const kafka = new Kafka({ brokers, logCreator: PinoLogCreator });
   const consumer = kafka.consumer({ groupId });
   await consumer.connect();
