612654b28b3c8961f2f58f30991eaacedd483a45
DHG <duyhung8a2@gmail.com>
2024-03-17 16:03:07 +0700

    hot-fix: change docker mapping port to another port

--- mongodb-service/src/index.ts
+++ mongodb-service/src/index.ts
@@ -4,10 +4,11 @@ import { connectToMongoDB } from './mongo';
 import { processEvent } from './event';
 
 const MONGO_DOMAIN = process.env.MONGO_DOMAIN || 'localhost';
+const MONGO_PORT = process.env.MONGO_PORT || '27017';
 const KAFKA_DOMAIN = process.env.KAFKA_DOMAIN || 'localhost';
 const KAFKA_PORT = process.env.KAFKA_PORT || '29092';
 
-const mongoURI = `mongodb://dev:password@${MONGO_DOMAIN}:27017`;
+const mongoURI = `mongodb://dev:password@${MONGO_DOMAIN}:${MONGO_PORT}`;
 const kafkaBrokers = [`${KAFKA_DOMAIN}:${KAFKA_PORT}`];
 const consumerGroupId = 'mongodb-service';
 
