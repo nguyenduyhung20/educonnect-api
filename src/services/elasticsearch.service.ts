509779687ead189345c70c959ada1c02ada912fc
DHG <duyhung8a2@gmail.com>
2024-05-21 15:50:01 +0700

    chore: do not throw error when conflict elasticsearch

--- src/services/elasticsearch.service.ts
+++ src/services/elasticsearch.service.ts
@@ -2,14 +2,18 @@ import { esClient } from '../config/es-client';
 import { ELASTICSEARCH_POST_INDEX_NAME } from '../constants/constants';
 
 export const increaseESView = async (id: string) => {
-  await esClient.update({
-    index: ELASTICSEARCH_POST_INDEX_NAME,
-    id,
-    script: {
-      lang: 'painless',
-      source: 'ctx._source.view++'
-    },
-    refresh: false,
-    retry_on_conflict: 1
-  });
+  try {
+    await esClient.update({
+      index: ELASTICSEARCH_POST_INDEX_NAME,
+      id,
+      script: {
+        lang: 'painless',
+        source: 'ctx._source.view++'
+      },
+      refresh: false,
+      retry_on_conflict: 2
+    });
+  } catch (error) {
+    console.error('Elasticsearch update view error: ', error);
+  }
 };
