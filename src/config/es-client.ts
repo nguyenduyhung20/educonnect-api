727c78a80c5ef909873a87b55bebdb7b8d09aded
DHG <duyhung8a2@gmail.com>
2024-05-23 13:19:58 +0700

    chore: update elasticsearch url

--- src/config/es-client.ts
+++ src/config/es-client.ts
@@ -1,3 +1,3 @@
 import { Client } from '@elastic/elasticsearch';
 
-export const esClient = new Client({ node: 'http://localhost:9200' });
+export const esClient = new Client({ node: process.env.ES_URL ?? 'http://localhost:9200' });
