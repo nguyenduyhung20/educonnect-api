476dcb19973d6b23f2780f26fea930c3edcaed20
DHG <duyhung8a2@gmail.com>
2024-05-30 22:05:08 +0700

    feat: add performance test

--- tests/load-test.js
+++ tests/load-test.js
@@ -2,24 +2,28 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 export const options = {
-  // A number specifying the number of VUs to run concurrently.
-  vus: 10,
-  // A string specifying the total duration of the test run.
-  duration: '30s',
+  stages: [
+    {
+      duration: '10s',
+      target: 100
+    },
+    {
+      duration: '30s',
+      target: 100
+    },
+    {
+      duration: '10s',
+      target: 0
+    }
+  ],
 
-  // The following section contains configuration options for execution of this
-  // test script in Grafana Cloud.
-  //
-  // See https://grafana.com/docs/grafana-cloud/k6/get-started/run-cloud-tests-from-the-cli/
-  // to learn about authoring and running k6 test scripts in Grafana k6 Cloud.
-  //
   cloud: {
     // The ID of the project to which the test is assigned in the k6 Cloud UI.
     // By default tests are executed in default project.
     projectID: 3699134,
     // The name of the test in the k6 Cloud UI.
     // Test runs with the same name will be grouped.
-    name: 'smoke-test.js'
+    name: 'load-test.js'
   }
 };
 
