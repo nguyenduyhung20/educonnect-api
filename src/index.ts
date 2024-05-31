b98d32dce8bce30f6c5dc71efecbf16d31100d96
DHG <duyhung8a2@gmail.com>
2024-05-31 12:31:05 +0700

    feat: remove cron job text summarize

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,26 @@ import app from './app';
 import { PORT } from './config/index.config';
 import { logger } from './utils/logger';
 import { initializeRedisClient } from './config/redis-client';
-import cron from 'node-cron';
-import { handleSummarizeMostInteractPost } from './controllers/summarizePost.controller';
+// import cron from 'node-cron';
+// import { handleSummarizeMostInteractPost } from './controllers/summarizePost.controller';
 
 const startServer = () => {
   app.listen(PORT, () => logger.info(`Running server on port ${PORT}`));
 };
-const scheduleCronJob = () => {
-  cron.schedule('* * * * *', async () => {
-    logger.info('Running your cron job');
-    try {
-      await handleSummarizeMostInteractPost();
-    } catch (error: any) {
-      logger.warn(`Cronjob failed with: ${error.message}\n`, error);
-    }
-  });
-};
+// const scheduleCronJob = () => {
+//   cron.schedule('* * * * *', async () => {
+//     logger.info('Running your cron job');
+//     try {
+//       await handleSummarizeMostInteractPost();
+//     } catch (error: any) {
+//       logger.warn(`Cronjob failed with: ${error.message}\n`, error);
+//     }
+//   });
+// };
 const main = async () => {
   await initializeRedisClient();
   startServer();
-  scheduleCronJob();
+  // scheduleCronJob();
 };
 
 main().catch((error) => {
